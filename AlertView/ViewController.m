889c108b11cb5f233e6db5db9c0aef0cb119d070
rluispdev <rluisp@icloud.com>
2024-07-14 14:50:09 -0300

    Criar um alertView

--- AlertView/ViewController.m
+++ AlertView/ViewController.m
@@ -8,15 +8,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+// Declaração da variável de instância
+@property (nonatomic, strong) NSString *mensagem;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+[super viewDidLoad];
+
+// Inicializa a variável de instância
+self.mensagem = @"Esta é uma mensagem de alerta.";
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+[super viewDidAppear:animated];
+
+// Criar o alerta
+UIAlertController *alerta = [UIAlertController alertControllerWithTitle:@"Alerta"
+                                                             message:self.mensagem // Utiliza a variável de instância
+                                                      preferredStyle:UIAlertControllerStyleAlert];
+// Criar a ação "OK"
+UIAlertAction *acaoOK = [UIAlertAction actionWithTitle:@"OK"
+                                               style:UIAlertActionStyleDefault
+                                             handler:^(UIAlertAction *action) {
+                                                 // Código a ser executado quando "OK" é pressionado
+                                                 NSLog(@"O usuário clicou em OK");
+                                             }];
+
+// Adicionar a ação ao alerta
+[alerta addAction:acaoOK];
+
+// Apresentar o alerta
+[self presentViewController:alerta animated:YES completion:nil];
+}
 
 @end
